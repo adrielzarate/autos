691c19a5f4d79198fc8c3cae2648673160bbb41f
Adriel Zarate <adriel.zarate@centricdigital.com>
2017-12-04 08:40:27 -0300

    adding jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,14 @@ $(function() {
     var datePicker = $('.datepicker');
     if( datePicker.length ) $('.datepicker').datetimepicker({format: 'DD/MM/YYYY',locale: 'es'});
 
+    $("#returnInPlace").click(function() {
+        if($(this).is(':checked')) {
+            // visible
+            $('.sh-toggle').show();
+        } else {
+            // no visible
+            $('.sh-toggle').hide();
+        }
+    });
+
 });
