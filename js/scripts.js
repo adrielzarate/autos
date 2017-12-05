319bbeec1fd8f0e209f1e840dfc7db079ac06afd
Adriel Zarate <adriel.zarate@centricdigital.com>
2017-12-04 23:08:35 -0300

    all done v1.0

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,35 @@ $(function() {
     var datePicker = $('.datepicker');
     if( datePicker.length ) $('.datepicker').datetimepicker({format: 'DD/MM/YYYY',locale: 'es'});
 
-    $("#returnInPlace").click(function() {
+    $('#returnInPlace').click(function() {
         if($(this).is(':checked')) {
-            // visible
-            $('.sh-toggle').show();
-        } else {
-            // no visible
             $('.sh-toggle').hide();
+        } else {
+            $('.sh-toggle').show();
         }
     });
+
+    $('.adicionales .sh-toggle-btn').click(function() {
+        $(this).toggleClass('sh-toggle-shown');
+        if( !$(this).hasClass('sh-toggle-shown') ) {
+            $('.adicionales .sh-toggle-xs').hide();
+            $(this).text('MOSTRAR');
+        } else {
+            $('.adicionales .sh-toggle-xs').show();
+            $(this).text('OCULTAR');
+        }
+    });
+
+    $('.detalle .sh-toggle-btn').click(function() {
+        $(this).toggleClass('sh-toggle-hidden');
+        if( !$(this).hasClass('sh-toggle-hidden') ) {
+            $('.detalle .sh-toggle-xs').show();
+            $(this).text('Ocultar detalle');
+        } else {
+            $('.detalle .sh-toggle-xs').hide();
+            $(this).text('Mostrar detalle');
+        }
+    });
+
 
 });
