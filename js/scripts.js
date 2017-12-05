68dfdcc28fc9990a9d7f1963913beed21a45d4ba
Adriel Zarate <adriel.zarate@centricdigital.com>
2017-12-04 23:26:13 -0300

    adding main container class .lieber-carro

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
 $(function() {
 
-    var datePicker = $('.datepicker');
-    if( datePicker.length ) $('.datepicker').datetimepicker({format: 'DD/MM/YYYY',locale: 'es'});
+    var datePicker = $('.lieber-carro .datepicker');
+    if( datePicker.length ) $('.lieber-carro .datepicker').datetimepicker({format: 'DD/MM/YYYY',locale: 'es'});
 
-    $('#returnInPlace').click(function() {
+    $('.lieber-carro #returnInPlace').click(function() {
         if($(this).is(':checked')) {
-            $('.sh-toggle').hide();
+            $('.lieber-carro .sh-toggle').hide();
         } else {
-            $('.sh-toggle').show();
+            $('.lieber-carro .sh-toggle').show();
         }
     });
 
-    $('.adicionales .sh-toggle-btn').click(function() {
+    $('.lieber-carro .adicionales .sh-toggle-btn').click(function() {
         $(this).toggleClass('sh-toggle-shown');
         if( !$(this).hasClass('sh-toggle-shown') ) {
-            $('.adicionales .sh-toggle-xs').hide();
+            $('.lieber-carro .adicionales .sh-toggle-xs').hide();
             $(this).text('MOSTRAR');
         } else {
-            $('.adicionales .sh-toggle-xs').show();
+            $('.lieber-carro .adicionales .sh-toggle-xs').show();
             $(this).text('OCULTAR');
         }
     });
 
-    $('.detalle .sh-toggle-btn').click(function() {
+    $('.lieber-carro .detalle .sh-toggle-btn').click(function() {
         $(this).toggleClass('sh-toggle-hidden');
         if( !$(this).hasClass('sh-toggle-hidden') ) {
-            $('.detalle .sh-toggle-xs').show();
+            $('.lieber-carro .detalle .sh-toggle-xs').show();
             $(this).text('Ocultar detalle');
         } else {
-            $('.detalle .sh-toggle-xs').hide();
+            $('.lieber-carro .detalle .sh-toggle-xs').hide();
             $(this).text('Mostrar detalle');
         }
     });
